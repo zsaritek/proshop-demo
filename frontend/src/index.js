0a9e17cdbc3f966490b03f0a76076574e31c2b40
zsaritek <zsaritek@gmail.com>
2024-05-07 16:00:09 +0200

    product screen details page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,13 +11,15 @@ import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-  <Route path="/" element={<App />}>
-    <Route index={true} path="/" element={<HomeScreen />} />
-  </Route>
-)
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+    </Route>
+  )
 )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
