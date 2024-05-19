59097391fe118edf49888d2dcfa37c934f909868
zsaritek <zsaritek@gmail.com>
2024-05-19 22:06:21 +0200

    user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
-import jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 // @desc   Auth user & get token
 // @route    POST /api/users/login
@@ -11,7 +11,8 @@ const authUser = asyncHandler(async (req, res) => {
     const user = await User.findOne({ email });
     // jwt
     if (user && (await user.matchPassword(password))) {
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+        const token = jwt.sign({ userId: user._id }, process.env.
+            JWT_SECRET, {
             expiresIn: '30d'
         });
 
@@ -20,7 +21,7 @@ const authUser = asyncHandler(async (req, res) => {
             httpOnly: true,
             secure: process.env.NODE_ENV !== 'development',
             sameSite: 'strict',
-            maxAge:30 *24 * 60 * 60 * 1000 // 30 days
+            maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
         });
 
         res.json({
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import asyncHandler from "./asyncHandler";
+import asyncHandler from "./asyncHandler.js";
 import User from "../models/userModel.js";
 
 //protect routes
@@ -11,11 +11,9 @@ const protect = asyncHandler(async (req, res, next) => {
     if (token) {
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            req.user = await User.findById(decoded.userId).select
-                ('-password');
+            req.user = await User.findById(decoded.userId).select('-password');
             next();
         } catch (error) {
-            console.log(error);
             res.status(401);
             throw new Error('Not authorized, token failed');
         }
