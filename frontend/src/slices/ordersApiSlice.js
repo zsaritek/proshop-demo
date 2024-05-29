0dc44c39fb7468d008c6d243f768f16a76bb6982
zsaritek <zsaritek@gmail.com>
2024-05-29 16:16:13 +0200

    manage my profile screen with table

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -28,6 +28,12 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
                 url: PAYPAL_URL,
             }),
             keepUnusedDataFor: 5,
+        }),
+        getMyOrders: builder.query({
+            query: () => ({
+                url: `${ORDERS_URL}/mine`,
+            }),
+            keepUnusedDataFor: 5,
         })
     }),
 
@@ -36,4 +42,4 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
 export const { useCreateOrderMutation,
     useGetOrderDetailsQuery,
     usePayOrderMutation,
-    useGetPayPalClientIdQuery } = ordersApiSlice;
+    useGetPayPalClientIdQuery, useGetMyOrdersQuery } = ordersApiSlice;
