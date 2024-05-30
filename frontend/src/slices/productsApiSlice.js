8c1086d155c754aecc4071ca74fdf56bdd404827
zsaritek <zsaritek@gmail.com>
2024-05-30 13:34:20 +0200

    edit new product

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -14,9 +14,16 @@ export const productApiSlice = apiSlice.injectEndpoints({
                 url: `${PRODUCTS_URL}/${productId}`
             }),
             keepUnusedDataFor: 5,
+        }),
+
+        createProduct: builder.mutation({
+            query: (newProduct) => ({
+                url: PRODUCTS_URL,
+                method: 'POST',
+            }),
+            invalidatesTags: ['Product'],
         })
     }),
-
 });
 
-export const { useGetProductsQuery, useGetProductDetailsQuery } = productApiSlice;
+export const { useGetProductsQuery, useGetProductDetailsQuery,useCreateProductMutation, } = productApiSlice;
