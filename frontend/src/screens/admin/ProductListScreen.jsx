a8d46f7118d4860af9dec149b1a173af0252daa8
zsaritek <zsaritek@gmail.com>
2024-06-07 16:45:58 +0200

    add pagination in productlist

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,14 +1,19 @@
 import { LinkContainer } from 'react-router-bootstrap';
 import { Table, Button, Row, Col } from 'react-bootstrap';
 import { FaEdit, FaTrash } from 'react-icons/fa';
+import { useParams } from 'react-router-dom';
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
+import Paginate from '../../components/Paginate';
 import { toast } from 'react-toastify';
 import { useGetProductsQuery, useCreateProductMutation, useDeleteProductMutation } from '../../slices/productsApiSlice';
 
+// we deleted products due to pagination set up in backend and updated map
 
 const ProductListScreen = () => {
-    const { data: products, isLoading, error, refetch } = useGetProductsQuery();
+    const { pageNumber } = useParams();
+
+    const { data, isLoading, error, refetch } = useGetProductsQuery({ pageNumber });
 
     const [createProduct, { isLoading: loadingCreate }] = useCreateProductMutation();
 
@@ -65,7 +70,7 @@ const ProductListScreen = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {products.map((product) => (
+                        {data.products.map((product) => (
                             <tr key={product._id}>
                                 <td>{product._id}</td>
                                 <td>{product.name}</td>
@@ -87,6 +92,7 @@ const ProductListScreen = () => {
 
                     </tbody>
                 </Table>
+                <Paginate pages={data.pages} page={data.page} isAdmin={true} />
             </>
         )}
     </>
