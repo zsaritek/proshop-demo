d3d57780ec9abfbeedb875a6fca767c2459e564f
zsaritek <zsaritek@gmail.com>
2024-06-11 09:34:38 +0200

    npm build,

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    // any route that is not api will be redirected to index.html
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...');
+    });
+}
+
 //Cookie parser middleware
 app.use(cookieParser());
 
